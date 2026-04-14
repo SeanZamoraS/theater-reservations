40044ec8d5923875917268803e2aa6b49d8cdbc5
Sean Zamora <zamoraseans@gmail.com>
2026-04-14 13:31:46 -0700

    almost done

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+
 import java.time.format.DateTimeFormatter;
 
 import java.util.Scanner;
@@ -15,38 +17,65 @@ public class TheaterReservations
     public static void promptUser()
     {
         String userName = " ";
-        String enteredDate = " ";
+        LocalDate formattedDate;
         int ticketAmount = 0;
-        //DateTimeFormatter formatter =
 
         System.out.println("""
                 Hello! Welcome to the theater reservations application.
                 
                 Please enter your name to continue: \n""");
+
         userName = receiveInputString();
 
         System.out.printf("\nAlright, %s. \n", userName);
         System.out.println("What date are you coming to the theater? (MM/DD/YYYY) \n");
-        enteredDate = receiveInputString();
-        DateTimeFormatter formattedDate;
+        //enteredDate = receiveInputString();
+
+        formattedDate = formatDateFromString();
 
 
-        System.out.printf("\nSo you'll be coming %s, great!\n", enteredDate);
+        System.out.printf("\nSo you'll be coming %s, great!\n", formattedDate);
         System.out.println("Lastly, how many tickets would you like? \n");
         ticketAmount = recieveInputInteger();
 
 
+
         System.out.printf("\nThank you for your purchase! \n%d tickets reserved for %s on %s.",
-                ticketAmount, userName, enteredDate);
-
-
+                ticketAmount, userName, formattedDate);
 
     }
+
     public static String receiveInputString()
     {
         String ticketString = input.nextLine()
                 .strip();
         return ticketString;
+    }
+
+    public static LocalDate formatDateFromString()
+    {
+        while (true)
+        {
+            try
+            {
+                String enteredDate = input.nextLine();
+
+                DateTimeFormatter formatEntry = DateTimeFormatter.ofPattern("M/d/yyyy");
+                //DateTimeFormatter formatForOutput = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy");
+                LocalDate formattedDate = LocalDate.parse(enteredDate, formatEntry);
+
+
+
+                return formattedDate;
+            }
+            catch (Exception e)
+            {
+                System.out.println("Please enter a valid date (MM/DD/YYYY):\n");
+            }
+        }
+
+
+
     }
     public static int recieveInputInteger()
     {
